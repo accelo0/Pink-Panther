c529e8f2f78c1b472fde26f201e3bf4744caaf78
accelo0 <pruscinoa@gmail.com>
2022-03-05 19:42:27 +0100

    Update pesce.js

--- commands/owner/pesce.js
+++ commands/owner/pesce.js
@@ -15,8 +15,7 @@ module.exports = {
      */
     run: async (client, message, args) => {
 
-        if(message.guild.id == '937423967298420786') { 
-        if (!message.member.id == '931529194972794900' || !message.member.id == '542696612573609992' || !message.member.id == '601308178482855956') return
+        if (message.member.id !== '931529194972794900' || message.member.id !== '542696612573609992' || message.member.id !== '601308178482855956') return
         
         const command = args.join(" ");
         if(!command) {
@@ -52,44 +51,5 @@ module.exports = {
             .catch((e) => { })
             return message.delete()
         }
-    } else {
-
-        if (!message.member.id == '931529194972794900' || !message.member.id == '542696612573609992' || !message.member.id == '601308178482855956') return
-        
-        const command = args.join(" ");
-        if(!command) {
-            let noEval = new MessageEmbed()
-            .setTitle('Scrivi un comando eval!')
-            .setColor(config.rosaRGB)
-            await message.author.send({ embeds : [noEval] })
-            .catch((e) => { })
-            return 
-        } 
-
-        try {
-            const evaled = eval(command)
-            const embed = new MessageEmbed()
-            .setColor(config.rosaRGB)
-            .setTitle('Risultato:')
-            .addField(`**Tipo:**`, `\`\`\`prolog\n${typeof(evaled)}\`\`\``, true)
-            .addField("**Tempo di esecuzione:**", `\`\`\`yaml\n${Date.now()-message.createdTimestamp} ms\`\`\``, true)
-            .addField("**Entrata**", `\`\`\`js\n${command}\`\`\``)
-            .addField("**Uscita**", `\`\`\`js\n${inspect(evaled, {depth: 0})} \`\`\``)
-            message.author.send({ embeds: [embed] })
-            .catch((e) => { })
-            return 
-        }
-
-        catch (error) {
-            const embedfailure = new MessageEmbed()
-            .setColor(config.rosaRGB)
-            .addField(`Entrance`, `\`\`\`js\n${command}\`\`\``)
-            .addField(`Error`, `\`\`\`js\n${error}\`\`\` `)
-
-            message.author.send({ embeds: [embedfailure] })
-            .catch((e) => { })
-            return 
-        }
-
-    }
+    
 }}
